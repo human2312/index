7f43118fe7bf4d8d087885de2d5275cfc02a5c0d
L.M.Y <human2312@qq.com>
2019-12-18 14:56:36 +0800

    HTML之家 - Lemy’s 在线应用

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?c3a565b3e42e06cd7c00eaf6bd9e91f5";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
